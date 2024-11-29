b67092fc0e8d4cce7605bff4a5949141362aa7d7
jbbok <qkrwjd94@gmail.com>
2024-11-29 16:42:37 +0900

    241129 bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,19 @@
+import { response } from "express";
 import User from "../models/user";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postJoin = async (req, res) => {
   const { email, username, password, name, location } = req.body;
+  const pageTitle = "join";
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already TrackEvent.",
+    });
+  }
+
   await User.create({
     email,
     username,
@@ -12,6 +23,7 @@ export const postJoin = async (req, res) => {
   });
   return res.redirect("/login");
 };
+
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove");
 export const login = (req, res) => res.send("login");
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -21,6 +22,10 @@ const userSchema = new mongoose.Schema({
   location: {
     type: String,
   },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
